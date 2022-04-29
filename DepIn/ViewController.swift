673f421bb393a7cd5b887c21a825fd4bc7b595e1
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-29 11:41:00 +0430

    Initial Commit

---
+++ DepIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DepIn
+//
+//  Created by Ali Aghamirbabaei on 4/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
